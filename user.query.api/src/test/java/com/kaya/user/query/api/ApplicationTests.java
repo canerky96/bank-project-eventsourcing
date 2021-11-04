f7feda33468a286fbe00b517124d9a79e3b7548a
Caner Kaya <canerky96@gmail.com>
2021-11-04 03:33:48 +0300

    initial projects added.

---
+++ user.query.api/src/test/java/com/kaya/user/query/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaya.user.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
