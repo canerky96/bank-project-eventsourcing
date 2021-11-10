e0f24ec13b2f900251d682440d07bf787197e6c8
Caner Kaya <caner.kaya@bilyoner.com>
2021-11-10 16:40:57 +0300

    mongo configs added.

--- user-management/user.cmd.api/src/main/java/com/kaya/user/cmd/api/UserCommandApplication.java
+++ user-management/user.cmd.api/src/main/java/com/kaya/user/cmd/api/UserCommandApplication.java
@@ -1,13 +1,15 @@
 package com.kaya.user.cmd.api;
 
+import com.kaya.user.core.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCommandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserCommandApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserCommandApplication.class, args);
+  }
 }
--- user-management/user.core/src/main/java/com/kaya/user/core/config/AxonConfig.java
+++ user-management/user.core/src/main/java/com/kaya/user/core/config/AxonConfig.java
@@ -3,13 +3,17 @@ package com.kaya.user.core.config;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.MongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoFactory;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoSettingsFactory;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
 import org.axonframework.serialization.Serializer;
+import org.axonframework.spring.config.AxonConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,4 +54,19 @@ public class AxonConfig {
         .build();
   }
 
+  @Bean
+  public EventStorageEngine storageEngine(MongoClient mongoClient) {
+    return MongoEventStorageEngine.builder()
+        .mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(mongoClient).build())
+        .build();
+  }
+
+  @Bean
+  public EmbeddedEventStore eventStore(
+      EventStorageEngine storageEngine, AxonConfiguration configuration) {
+    return EmbeddedEventStore.builder()
+        .storageEngine(storageEngine)
+        .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
+        .build();
+  }
 }
